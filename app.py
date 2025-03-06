18890df85747bcb73b7b871bf8c8d6652ed7da61
cbks95 <157106471+cbks95@users.noreply.github.com>
2025-03-06 18:22:30 +0100

    Add files via upload

---
+++ app.py
@@ -0,0 +1,16 @@
+from fastai.vision.widgets import *
+import gradio as gr
+
+learn_inf = load_learner('export.pkl')
+
+categories = tuple(learn_inf.dls.vocab)
+
+def classify_image(img):
+    pred, idx, probs = learn_inf.predict(img)
+    return dict(zip(categories, map(float,probs)))
+
+image = gr.inputs.Image(shape=(192,192))
+label = gr.outputs.Label()
+examples = ['images/good_food.jpg', 'images/bad_food.jpg']
+
+gr.Interface(fn=classify_image, inputs=image, outputs=label), examples=examples.launch(share=True)
