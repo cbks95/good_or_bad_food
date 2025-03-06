d069ea0dbc2e37b11c5322d75f2a5a51690d6fe2
cbks95 <157106471+cbks95@users.noreply.github.com>
2025-03-06 19:35:32 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,16 +1,18 @@
-from fastai.vision.widgets import *
+from fastai.vision.all import *
 import gradio as gr
 
 learn_inf = load_learner('export.pkl')
 
-categories = tuple(learn_inf.dls.vocab)
+#categories = tuple(learn_inf.dls.vocab)
+categories = ("Ungesundes 'Essen'", "Echtes Essen")
 
 def classify_image(img):
     pred, idx, probs = learn_inf.predict(img)
     return dict(zip(categories, map(float,probs)))
 
-image = gr.inputs.Image(shape=(192,192))
-label = gr.outputs.Label()
+image = gr.Image()
+label = gr.Label()
 examples = ['good_food.jpg', 'bad_food.jpg']
 
-gr.Interface(fn=classify_image, inputs=image, outputs=label), examples=examples.launch(share=True)
+intf = gr.Interface(fn=classify_image, inputs=image, outputs=label, examples=examples)
+intf.launch(share=True)
